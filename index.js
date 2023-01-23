984fd6ba33602e98fedc29ae69f950f46e9f579c
Khalil Merine <merinekh@gmail.com>
2023-01-23 17:00:45 -0500

    Git ignore + .env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 var cors = require("cors");
 
 app.use(cors());
