c6de928df2ded7261605c87d25ebef8e898fb90e
Nick Abate <nick.abate@outlook.com>
2023-01-25 21:03:16 -0500

    Implement knex, migrate and seed.

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const fs = require("node:fs");
 
 //Convenience function to grab data
-const getWarehouseItems = (id) => {
-  const warehouseFromFile = fs.readFileSync(
-    `./data/dummy-warehouse-${id}.json`
-  );
-  return JSON.parse(warehouseFromFile);
-};
+// const getWarehouseItems = (id) => {
+//   const warehouseFromFile = fs.readFileSync(
+//     `./data/dummy-warehouse-${id}.json`
+//   );
+//   return JSON.parse(warehouseFromFile);
+// };
 
 //Return specific warehouse items by ID
+
 router.get("/:id/inventories", (req, res) => {
   let id = req.params.id;
   const warehouse = getWarehouseItems(id);
