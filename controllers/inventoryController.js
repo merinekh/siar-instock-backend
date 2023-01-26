9abcc6c85192fc8325a119db8010aedcf8c0fbdb
Nick Abate <nick.abate@outlook.com>
2023-01-26 09:37:29 -0500

    Create inventory by ID get request.

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -9,3 +9,22 @@ exports.index = (_req, res) => {
       res.status(400).send(`Error retrieving Inventories: ${err}`)
     );
 };
+
+exports.singleInventoryID = (req, res) => {
+  knex("inventories")
+    .where({ id: req.params.id })
+    .then((data) => {
+      // If record is not found, respond with 404
+      if (!data.length) {
+        return res
+          .status(404)
+          .send(`Record with id: ${req.params.id} is not found`);
+      }
+      res.status(200).json(data[0]);
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .send(`Error retrieving Inventory item ID ${req.params.id} ${err}`)
+    );
+};
