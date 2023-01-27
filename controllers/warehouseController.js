5074fc46e8f57fcfb92d1f517efaac8ccadc18e6
Darnell Eudoxie <darnelleudoxie@gmail.com>
2023-01-26 22:53:47 -0400

    single warehouse backend almost done

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -9,7 +9,21 @@ exports.index = (_req, res) => {
       res.status(400).send(`Error retrieving Warehouses: ${err}`)
     );
 };
-
+exports.singleWarehouse = (req, res) => {
+  knex("warehouses")
+    .where({ id: req.params.id })
+    .then((data) => {
+      if (!data.length) {
+        return res
+          .status(404)
+          .send(`Record with id: ${req.params.id} is not found`);
+      }
+      res.status(200).json(data[0]);
+    })
+    .catch((err) =>
+      res.status(400).send(`Error retrieving warehouse ${req.params.id} ${err}`)
+    );
+};
 exports.singleWarehouseInventories = (req, res) => {
   knex
     .select("warehouses.*", "inventories.*")
